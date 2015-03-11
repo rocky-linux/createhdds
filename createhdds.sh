95aa6800648df1737011af00c5fb0dc2b7ecc4fc
Garret Raziel <boloomka@gmail.com>
2015-03-11 16:19:20 +0100

    creating new HDDs

--- createhdds.sh
+++ createhdds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Creating disk_full.img..."
 guestfish <<_EOF_
 sparse disk_full.img 10G
 run
@@ -15,6 +16,7 @@ write /testfile "Oh, hi Mark"
 umount /
 _EOF_
 
+echo "Creating disk_freespace.img..."
 guestfish <<_EOF_
 sparse disk_freespace.img 10G
 run
@@ -23,4 +25,22 @@ part-add /dev/sda p 4096 2097152
 mkfs ext4 /dev/sda1
 mount /dev/sda1 /
 write /testfile "Hello, world!"
+_EOF_
+
+echo "Creating fedora21.img..."
+# TODO: it should be possible to create updated image, but there is a bug, see https://bugzilla.redhat.com/show_bug.cgi?id=1084221
+# so you are required to manually boot this image and run 'yum -y update'
+#virt-builder fedora-21 -o fedora21.img --update --selinux-relabel --root-password password:weakpassword
+virt-builder fedora-21 -o fedora21.img --root-password password:weakpassword
+
+echo "Creating disk_ks.img..."
+curl -o "/tmp/root-user-crypted-net.ks" "https://jskladan.fedorapeople.org/kickstarts/root-user-crypted-net.ks"
+guestfish <<_EOF_
+sparse disk_ks.img 100MB
+run
+part-init /dev/sda mbr
+part-add /dev/sda p 4096 -1
+mkfs ext4 /dev/sda1
+mount /dev/sda1 /
+upload /tmp/root-user-crypted-net.ks /root-user-crypted-net.ks
 _EOF_
