707eb5e83765dd3d9a1eba322d4f06fa580a8f93
Adam Williamson <awilliam@redhat.com>
2021-06-01 13:35:42 -0700

    Update copyright statement to current RH recommendation
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- createhdds.py
+++ createhdds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2015 Red Hat
+# Copyright Red Hat
 #
 # createhdds is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
