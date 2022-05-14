824de0a3f8f73e6165d874281dc2c9dd31028685
Trevor Cooper <tcooper@rockylinux.org>
2022-05-14 13:23:19 -0700

    correct 'unknown' os-variant for Rocky 8

--- createhdds.py
+++ createhdds.py
@@ -265,7 +265,7 @@ class VirtInstallImage(object):
         if shortid not in out:
             # this will just use the most recent rocky release number
             # virt-install / osinfo knows
-            shortid = 'rocky-unknown'
+            shortid = 'rocky8-unknown'
 
         # destroy and delete the domain we use for all virt-installs
         conn = libvirt.open()
